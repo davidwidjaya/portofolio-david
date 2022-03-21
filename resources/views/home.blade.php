f267ee01c388073bbc04d33ce023be12189442ef
davidwidjaya <davidpro71@gmail.com>
2022-03-21 19:22:47 +0700

    initial commit and home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,118 @@
+@extends('layout.layout')
+
+@section('section')
+    <div class="section-header fade-in animate-bottom container-fluid">
+        <div class="container">
+            <div class="row">
+                {{-- col-12 col-lg-5 col-md-5 banner-new-homepage --}}
+                <div class="col-12 col-lg-5 d-flex justify-content-center align-items-center">
+                    <div class="col">
+                        <h1 class="font-bold">Build, Grow, and Expand Your Futures Startup with Us!</h1>
+                        <div>We provide our best service for you with 24-hour consultation, unlimited revisions, beautiful
+                            UI/UX for application or website for attract more customer for you.</div>
+                            <br>
+                        <button class="button-82-pushable" role="button">
+                            <span class="button-82-shadow"></span>
+                            <span class="button-82-edge"></span>
+                            <span class="button-82-front text">
+                                See More
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-5">
+                    <div class="service_splash__18SnM">
+                        <div>
+                            <div class="service-splash_decor__2nsyu service-splash_circle__3ssSu">
+                                <div class="service-splash_decorBG__3vp0y service-splash_hides__1FsgH"
+                                    style="background:#457af5;border-radius:unset;clip-path:circle(40%)"></div><svg
+                                    width="100" height="100" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg"
+                                    class="service-splash_decorCircle1__2mcjT service-splash_bob__1wHOM service-splash_hides__1FsgH"
+                                    fill="#f55c69">
+                                    <circle cx="25" cy="25" r="25"></circle>
+                                </svg><svg width="100" height="100" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg"
+                                    class="service-splash_decorCircle2__2xlPF service-splash_bob__1wHOM service-splash_hides__1FsgH"
+                                    style="animation-delay:0.2s" fill="#ffb52e">
+                                    <circle cx="25" cy="25" r="25"></circle>
+                                </svg><svg width="100" height="100" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg"
+                                    class="service-splash_decorCircle3__29Uto service-splash_bob__1wHOM service-splash_hides__1FsgH"
+                                    style="animation-delay:0.4s" fill="#f55c69">
+                                    <circle cx="25" cy="25" r="25"></circle>
+                                </svg>
+                            </div><video class="service-splash_video__py_FT" preload="auto" autoplay playsinline="" muted=""
+                                loop="" disableremoteplayback="">
+                                <source
+                                    src="https://cdn.sanity.io/files/kbz1tcxy/production/bc0e95417e2c8d0930199f0b46eed3da27587ea4.webm"
+                                    type="video/webm">
+                                <source
+                                    src="https://cdn.sanity.io/files/kbz1tcxy/production/4ecbc314aed392e08254fe26d28ee8d5c0442782.mov"
+                                    type="video/quicktime">
+                            </video>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+    <div class="section-header fade-in animate-bottom container-fluid">
+        <div class="container">
+            <div class="row">
+                {{-- col-12 col-lg-5 col-md-5 banner-new-homepage --}}
+                <div class="col-12 col-lg-5 d-flex justify-content-center align-items-center">
+                    <div class="col">
+                        <h1 class="font-bold">Build, Grow, and Expand Your Futures Startup with Us!</h1>
+                        <div>We provide our best service for you with 24-hour consultation, unlimited revisions, beautiful
+                            UI/UX for application or website for attract more customer for you.</div>
+                            <br>
+                        <button class="button-82-pushable" role="button">
+                            <span class="button-82-shadow"></span>
+                            <span class="button-82-edge"></span>
+                            <span class="button-82-front text">
+                                See More
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-5">
+                    <div class="service_splash__18SnM">
+                        <div>
+                            <div class="service-splash_decor__2nsyu service-splash_circle__3ssSu">
+                                <div class="service-splash_decorBG__3vp0y service-splash_hides__1FsgH"
+                                    style="background:#457af5;border-radius:unset;clip-path:circle(40%)"></div><svg
+                                    width="100" height="100" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg"
+                                    class="service-splash_decorCircle1__2mcjT service-splash_bob__1wHOM service-splash_hides__1FsgH"
+                                    fill="#f55c69">
+                                    <circle cx="25" cy="25" r="25"></circle>
+                                </svg><svg width="100" height="100" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg"
+                                    class="service-splash_decorCircle2__2xlPF service-splash_bob__1wHOM service-splash_hides__1FsgH"
+                                    style="animation-delay:0.2s" fill="#ffb52e">
+                                    <circle cx="25" cy="25" r="25"></circle>
+                                </svg><svg width="100" height="100" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg"
+                                    class="service-splash_decorCircle3__29Uto service-splash_bob__1wHOM service-splash_hides__1FsgH"
+                                    style="animation-delay:0.4s" fill="#f55c69">
+                                    <circle cx="25" cy="25" r="25"></circle>
+                                </svg>
+                            </div><video class="service-splash_video__py_FT" preload="auto" autoplay playsinline="" muted=""
+                                loop="" disableremoteplayback="">
+                                <source
+                                    src="https://cdn.sanity.io/files/kbz1tcxy/production/bc0e95417e2c8d0930199f0b46eed3da27587ea4.webm"
+                                    type="video/webm">
+                                <source
+                                    src="https://cdn.sanity.io/files/kbz1tcxy/production/4ecbc314aed392e08254fe26d28ee8d5c0442782.mov"
+                                    type="video/quicktime">
+                            </video>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+@endsection
+
+
+@push('script')
+@endpush
---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>David Alexander W - Portofolio</title>
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+
+    {{-- Bootstrap 5 --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    {{-- <link rel='preload' href='https://fonts.googleapis.com/css?family=Quicksand:400,500,700&display=swap' as='style' onload="this.rel='stylesheet'">
+    <noscript><link rel="stylesheet" href='https://fonts.googleapis.com/css?family=Quicksand:400,500,700&display=swap'></noscript>
+        <link rel='preload' href='https://use.fontawesome.com/releases/v5.6.3/css/all.css' as='style' onload="this.rel='stylesheet'">
+    <noscript><link rel="stylesheet" href='https://use.fontawesome.com/releases/v5.6.3/css/all.css'></noscript> --}}
+
+</head>
+<body>
+    {{-- <h1 class="font-medium">Ini makanan datang.</h1> --}}
+    @include('layout.header')
+    @yield('section')
+    @include('layout.footer')
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    @stack('script')
+</body>
+</html>
